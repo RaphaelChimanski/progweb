c2cde23c5f942cae0f3e8efe368b7f4d0bd25e9a
RaphaelChimanski <rachimanski@hotmail.com>
2021-10-31 22:42:55 -0300

    Adicionando a pasta de JavaScript

---
+++ js/carregarItens.js
@@ -0,0 +1,62 @@
+var conteudoLocal = [ [0, "imagens/loot1.jpg", "LootBox1", 300, false], [1, "imagens/loot2.jpg", "LootBox2", 150, false], [2, "imagens/loot3.jpg", "LootBox3", 2000, false] ];
+var carrinho = [];
+
+var storage = window.localStorage;
+
+storage.setItem("itens", JSON.stringify(conteudoLocal));
+
+
+window.onload = function(){
+	
+	
+	
+	montarCardProdutos();
+}
+
+function comprar(id){
+		console.log("Comprou!: " + conteudoLocal[id][2]);
+		conteudoLocal[id][4] = true;
+		carrinho.push(conteudoLocal[id]);
+		window.localStorage.setItem("ListaCompras", JSON.stringify(carrinho))
+		montarCardProdutos();
+	}
+
+function montarCardProdutos(){
+	
+	document.getElementById("inserirCards").innerHTML = "";
+	
+	
+	for(var i = 0; i < conteudoLocal.length; i++){
+		
+
+			var conteudo = "";
+
+			conteudo += '<div class="card">';
+			conteudo += '<div class="card-img">';
+			conteudo += '<img src="' + conteudoLocal[i][1]  + '">';
+			conteudo += '</div>';
+			conteudo += '<div class="card-descricao">';
+			conteudo += '<h5>' + conteudoLocal[i][2] + '</h5>';
+			conteudo += '</div>';
+			conteudo += '<div class="card-preco">';
+			conteudo += '<h5>' + conteudoLocal[i][3] + '</h5>';
+			conteudo += '</div>';
+
+			if(conteudoLocal[i][4] == false){
+				conteudo += '<div class="card-botao">';
+				conteudo += '<input type="button" onclick="comprar(' + conteudoLocal[i][0]  + ')" value="Comprar">';
+				conteudo += '</div>';	
+			}
+			else{
+				conteudo += '<div class="noCarrinho">';
+				conteudo += '<input type="submit" value="No Carrinho">';
+				conteudo += '</div>';
+			}
+		
+		
+		document.getElementById("inserirCards").innerHTML += conteudo;
+	}
+	
+	
+	
+}
